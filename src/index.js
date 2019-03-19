e14ee9744ea16182cc976bf8257a21256bb62ca1
AxelE <axel91evrard@gmail.com>
2019-03-19 23:06:44 +0100

    French Hello World

--- src/index.js
+++ src/index.js
@@ -6,4 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+const element = <h1>Bonjour héro</h1>;
+ReactDOM.render(element, document.getElementById('test'));
+
 serviceWorker.register();
