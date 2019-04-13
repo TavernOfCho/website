06215c9ef7ee73ffa8d30127c73c3683589eb409
roman <roman.dilmi@orange.fr>
2019-04-13 15:26:16 +0200

    #21 - Fix form server name

--- src/components/CharacterForm.js
+++ src/components/CharacterForm.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-
 
 const styles = theme => ({
   root: {
@@ -22,41 +22,54 @@ const styles = theme => ({
   },
 });
 
-
 class CharacterForm extends React.Component {
-
   state = {
     server: '',
     labelWidth: 0,
   };
+
+  componentDidMount() {
+    this.setState({
+      labelWidth: ReactDOM.findDOMNode(this.InputLabelRef).offsetWidth,
+    });
+  }
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
   render() {
-
     const { classes } = this.props;
 
     return (
       <form className={classes.root} autoComplete="off">
-        <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="Serveur">Serveur</InputLabel>
-            <Select
-              value={this.state.age}
-              onChange={this.handleChange}
-              /*inputProps={{
-                name: 'age',
-                id: 'age-simple',
-              }}*/
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
+        <FormControl variant="outlined" className={classes.formControl}>
+          <InputLabel
+            ref={ref => {
+              this.InputLabelRef = ref;
+            }}
+            htmlFor="outlined-server-simple"
+          >
+            Serveur
+          </InputLabel>
+          <Select
+            value={this.state.server}
+            onChange={this.handleChange}
+            input={
+              <OutlinedInput
+                labelWidth={this.state.labelWidth}
+                name="server"
+                id="outlined-server-simple"
+              />
+            }
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
         </FormControl>
       </form>
     );
