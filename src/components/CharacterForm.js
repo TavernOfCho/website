560bfe48068c2689b9e3193fdb63da76e5eb2c57
roman <roman.dilmi@orange.fr>
2019-04-14 13:06:56 +0200

    #21 - Select for servers with some servers fetch in api/realms

--- src/components/CharacterForm.js
+++ src/components/CharacterForm.js
@@ -8,6 +8,11 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
+import List from "@material-ui/core/List/List";
+import ListItem from "@material-ui/core/ListItem/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon/ListItemIcon";
+import InboxIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import ListItemText from "@material-ui/core/ListItemText/ListItemText";
 
 const styles = theme => ({
   root: {
@@ -46,7 +51,7 @@ class CharacterForm extends React.Component {
 
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/ld+json");
-    myHeaders.append("Authorization", "Bearer "+ "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE1NTUxODMzODMsImV4cCI6MTU1NTIxOTM4Mywicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoiam9obiJ9.hc6QgDcIEwPfHOTR6hTQSHRn0mlq5sellTGZHrVyDHPH2MB4OWuauZ4887GNqn6eGyTm52h5hWBeJrUvbQwYgw");
+    myHeaders.append("Authorization", "Bearer "+ "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE1NTUyMzUwNzgsImV4cCI6MTU1NTI3MTA3OCwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoiam9obiJ9.HD6boI2zBPr9SGHMO1zegSDFAYWM0FUOjIvGgm-PGtCzxza-ak-OgbeLMJWXt3dW1S5xh-N5umFklj2wzjwRXQ");
     fetch('https://127.0.0.1:8052/realms',
       {
         method: 'GET',
@@ -56,9 +61,6 @@ class CharacterForm extends React.Component {
       .then(response => response.json())
       .then(data => {
         this.setState({servers: data["hydra:member"]});
-
-        console.log(this.getServerNames());
-
         }
       )
 
@@ -75,50 +77,53 @@ class CharacterForm extends React.Component {
   render() {
     const { classes } = this.props;
 
-    console.log('render:' ,this.state.servers);
+    const serversNames = this.getServerNames();
+
+    const selectServers = (
+      <Select
+        value={this.state.server}
+        onChange={this.handleChangeServer}
+        input={
+          <OutlinedInput
+            labelWidth={this.state.labelWidth}
+            name="server"
+            id="outlined-server-simple"
+          />
+        }
+      >
+        {serversNames.map((name,index) => (
+          <MenuItem value={name} key={index}>{name}</MenuItem>
+        ))}
+      </Select>
+    );
 
     return (
-      <form autoComplete="off">
-        <FormControl variant="outlined" className={classes.formControl}>
-          <InputLabel
-            ref={ref => {
-              this.InputLabelRef = ref;
-            }}
-            htmlFor="outlined-server-simple"
-          >
-            Serveur
-          </InputLabel>
-          <Select
-            value={this.state.server}
-            onChange={this.handleChangeServer}
-            input={
-              <OutlinedInput
-                labelWidth={this.state.labelWidth}
-                name="server"
-                id="outlined-server-simple"
-              />
-            }
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
+      <div>
+        <form autoComplete="off">
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel
+              ref={ref => {
+                this.InputLabelRef = ref;
+              }}
+              htmlFor="outlined-server-simple"
+            >
+              Serveur
+            </InputLabel>
+            {selectServers}
 
 
 
-        </FormControl>
-          <TextField
-            id="standard-name"
-            label="Nom du personnage"
-            className={classes.textField}
-            value={this.state.name}
-            onChange={this.handleChangeName('name')}
-            margin="normal"
-          />
-      </form>
+          </FormControl>
+            <TextField
+              id="standard-name"
+              label="Nom du personnage"
+              className={classes.textField}
+              value={this.state.name}
+              onChange={this.handleChangeName('name')}
+              margin="normal"
+            />
+        </form>
+      </div>
     );
   }
 }
