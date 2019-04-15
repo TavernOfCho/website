8e917b2e3d7473a504337ed1c7a4abdd12528d93
AxelE <axel91evrard@gmail.com>
2019-04-15 11:00:30 +0200

    Resolve #22 | Deploy with Firebase

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ docker-compose run website
 ```
 
 Open [http://127.0.0.1:8054](http://127.0.0.1:8054) to view it in the browser.
+
+
+## FIREBASE
+
+```
+npm run-script build
+docker-compose exec website firebase login
+docker-compose exec website firebase deploy
+```
+
+Open [https://tavernedecho.firebaseapp.com/] (https://tavernedecho.firebaseapp.com/) 
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.9.4/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAhAclqaURfECn3ICcmX0RbHzm7MgDQd5w",
+    authDomain: "tavernedecho.firebaseapp.com",
+    databaseURL: "https://tavernedecho.firebaseio.com",
+    projectId: "tavernedecho",
+    storageBucket: "tavernedecho.appspot.com",
+    messagingSenderId: "908886937216"
+  };
+  firebase.initializeApp(config);
+</script>
