874644c14512c0a50f544ccc021f2c8c3aa4094e
AxelE <axel91evrard@gmail.com>
2019-05-31 18:57:31 +0200

    Fix redirection

--- src/index.js
+++ src/index.js
@@ -22,8 +22,11 @@ var firstLocale = locale.split('-');
 
 var domain = "tavernofcho.com";
 
-if(document.domain != domain)
+if(document.domain !== "https://127.0.0.1:8054/"){
+  console.log('Salut les devs');
+}else if(document.domain !== domain){
   document.location.href="https://tavernofcho.com/";
+}
   
 const language = firstLocale[0];
 
