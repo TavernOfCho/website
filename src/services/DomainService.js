c70b5ceb05a15c384b81e957f8943848bb424609
Roman DILMI <roman.dilmi@orange.fr>
2019-06-14 15:38:44 +0200

    #67 Chat - Adding service for domains

---
+++ src/services/DomainService.js
@@ -0,0 +1,35 @@
+
+export const domainService = {
+  getApiDomain,
+  getMercureDomain,
+};
+
+const getApiDomain = () => {
+  let domain = document.domain;
+  let prodApiDomain = 'https://api.tavernofcho.com';
+  let devApiDomain = 'https://127.0.0.1:8052';
+
+  switch(domain) {
+    case '127.0.0.1':
+      return domain = devApiDomain;
+    case 'tavernofcho.com':
+      return domain = prodApiDomain;
+    default:
+      return domain = null;
+  }
+}
+
+const getMercureDomain = () => {
+  let domain = document.domain;
+  let prodMercureDomain = 'https://api.tavernofcho.com';
+  let devMercureDomain = 'https://127.0.0.1:8052';
+
+  switch(domain) {
+    case '127.0.0.1':
+      return domain = devMercureDomain;
+    case 'tavernofcho.com':
+      return domain = prodMercureDomain;
+    default:
+      return domain = null;
+  }
+}
