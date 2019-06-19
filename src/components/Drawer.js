96c80a117553027f1f2ac669bca995382c4d7416
AxelE <axel91evrard@gmail.com>
2019-06-19 22:38:06 +0200

    Fix responsive on traduction

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -282,7 +282,7 @@ class Drawer extends React.Component {
                     src={require("./img/flagus.png")}
                     alt="flag-en"
                   />
-                  <FormattedMessage id='english' defaultMessage='English' />
+                  <FormattedMessage id='english' defaultMessage='EN' />
                 </a>
                 <a href="#fr" className={classes.changeLang} onClick={() => this.changeLanguage("fr")}>
                   <img
@@ -290,7 +290,7 @@ class Drawer extends React.Component {
                     src={require("./img/flagfr.png")}
                     alt="flag-fr"
                   />
-                  <FormattedMessage id='french' defaultMessage='French' />
+                  <FormattedMessage id='french' defaultMessage='FR' />
                 </a>
               </div>
 
