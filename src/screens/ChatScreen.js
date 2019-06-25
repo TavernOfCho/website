72f272e833f53a3c628b06fc315a506677b0fa56
Roman DILMI <roman.dilmi@orange.fr>
2019-06-25 15:04:40 +0200

    #132 - Alert refacto - Chat

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -3,15 +3,44 @@ import HeroUnit from "../components/HeroUnit";
 import HeroBanner from "../components/HeroBanner";
 import { FormattedMessage } from 'react-intl';
 import ChatForm from "../components/form/ChatForm";
+import connect from "react-redux/es/connect/connect";
+import {history} from "../helpers/history";
+import {alertActions} from "../store/actions/alert";
+import ContextMessage from "../components/ContextMessage";
 
-export default class ChatScreen extends React.Component {
+class ChatScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    const { dispatch } = this.props;
+
+    history.listen((location, action) => {
+      // clear alert on location change
+      dispatch(alertActions.clear());
+    });
+  }
+
   render() {
+
+    const { alert } = this.props;
+
     return (
       <div>
         <HeroBanner title={<FormattedMessage id='chotavern' defaultMessage="Cho's Tavern" />} description={<FormattedMessage id='homescreen.welcome' defaultMessage="Let's choose the next achievement you want to do and find new friends" />}/>
         <HeroUnit title={<FormattedMessage id='chat' defaultMessage="Chat" />}/>
+        {alert.message && <ContextMessage message={alert.message} type={alert.type}/>}
         <ChatForm/>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  const { alert } = state;
+  return {
+    alert,
+  };
+}
+
+export default connect(mapStateToProps)(ChatScreen);
