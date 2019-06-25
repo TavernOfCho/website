ab332102e834b95234f02b06fc28994f9e3f0901
Roman DILMI <roman.dilmi@orange.fr>
2019-06-25 14:53:19 +0200

    Alert refacto - Achievement

--- src/screens/AchievementScreen.js
+++ src/screens/AchievementScreen.js
@@ -3,17 +3,45 @@ import HeroBanner from "../components/HeroBanner";
 import { FormattedMessage } from 'react-intl';
 import AchievementForm from "../components/form/AchievementForm";
 import Helper from "../components/feature/cho/Helper";
+import {history} from "../helpers/history";
+import {alertActions} from "../store/actions/alert";
+import connect from "react-redux/es/connect/connect";
+import ContextMessage from "../components/ContextMessage";
 
-export default class MountScreen extends React.Component {
+class AchievementScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { dispatch } = this.props;
+
+    history.listen((location, action) => {
+      // clear alert on location change
+      dispatch(alertActions.clear());
+    });
+  }
+
   render() {
+
+    const { alert } = this.props;
+
     return (
       <div>
-        <HeroBanner title={<FormattedMessage id='chotavern' defaultMessage="Cho's Tavern" />} 
+        <HeroBanner title={<FormattedMessage id='chotavern' defaultMessage="Cho's Tavern" />}
         description={<FormattedMessage id="title.achievement" defaultMessage="Do you want me to sing your legend?" />}/>
         <p><FormattedMessage id='character.helper' defaultMessage="Please fill your server and the name of your character." /></p>
+        {alert.message && <ContextMessage message={alert.message} type={alert.type}/>}
         <AchievementForm />
         <Helper />
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  const { alert } = state;
+  return {
+    alert,
+  };
+}
+
+export default connect(mapStateToProps)(AchievementScreen);
