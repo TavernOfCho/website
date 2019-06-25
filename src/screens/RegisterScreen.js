d0773be3000c28cd8a7d07318a170e554e57b4ef
Roman DILMI <roman.dilmi@orange.fr>
2019-06-25 15:18:38 +0200

    #132 - Alert refacto - Register

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,12 +1,42 @@
 import React from "react";
 import Register from "../components/Register";
+import {history} from "../helpers/history";
+import {alertActions} from "../store/actions/alert";
+import connect from "react-redux/es/connect/connect";
+import ContextMessage from "../components/ContextMessage";
 
-export default class RegisterScreen extends React.Component {
+class RegisterScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    const { dispatch } = this.props;
+
+    history.listen((location, action) => {
+      // clear alert on location change
+      dispatch(alertActions.clear());
+    });
+  }
+
   render() {
+
+    const { alert } = this.props;
+
     return (
       <div className="RegisterScreen">
+        {/* ---- Location for alert ---- */}
+        {alert.message && <ContextMessage message={alert.message} type={alert.type}/>}
         <Register/>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  const { alert } = state;
+  return {
+    alert,
+  };
+}
+
+export default connect(mapStateToProps)(RegisterScreen);
