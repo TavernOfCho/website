ed04c8da1eea3b8a79ca2a0fe2f3880b88a9392a
Roman DILMI <roman.dilmi@orange.fr>
2019-06-26 15:53:08 +0200

    #132 - Alert refacto auto hide duration - Help

--- src/screens/HelpScreen.js
+++ src/screens/HelpScreen.js
@@ -2,33 +2,17 @@ import React from "react";
 import HeroBanner from "../components/HeroBanner";
 import { FormattedMessage } from 'react-intl';
 import Help from "../components/feature/cho/Help";
-import { connect } from 'react-redux';
-import ContextMessage from "../components/ContextMessage";
 
-class HelpScreen extends React.Component {
+export default class HelpScreen extends React.Component {
 
   render() {
-
-    const { alert } = this.props;
 
     return (
       <div>
         <HeroBanner title={<FormattedMessage id='chotavern' defaultMessage="Cho's Tavern" />}
         description={<FormattedMessage id="title.help" defaultMessage="Lost? Don't worry!" />}/>
-        {/* ---- Location for alert ---- */}
-        {alert.message && <ContextMessage message={alert.message} type={alert.type}/>}
         <Help />
       </div>
     );
   }
 }
-
-function mapStateToProps(state) {
-  const { alert } = state;
-  return {
-    alert,
-  };
-}
-
-export default connect(mapStateToProps)(HelpScreen);
-
