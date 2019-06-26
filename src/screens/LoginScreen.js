e84df8e69b2febcea10645ccc8f214eb56ad5694
Roman DILMI <roman.dilmi@orange.fr>
2019-06-26 15:55:08 +0200

    #132 - Alert refacto auto hide duration - Register

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Login from "../components/Login";
 
-
 export default class LoginScreen extends React.Component {
 
   render() {
--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,29 +1,14 @@
 import React from "react";
 import Register from "../components/Register";
-import { connect } from 'react-redux';
-import ContextMessage from "../components/ContextMessage";
 
-class RegisterScreen extends React.Component {
+export default class RegisterScreen extends React.Component {
 
   render() {
 
-    const { alert } = this.props;
-
     return (
       <div className="RegisterScreen">
-        {/* ---- Location for alert ---- */}
-        {alert.message && <ContextMessage message={alert.message} type={alert.type}/>}
         <Register/>
       </div>
     );
   }
 }
-
-function mapStateToProps(state) {
-  const { alert } = state;
-  return {
-    alert,
-  };
-}
-
-export default connect(mapStateToProps)(RegisterScreen);
