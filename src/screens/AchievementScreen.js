b38292d7ec2e6cafe0355269afc20cf39608ab99
Roman DILMI <roman.dilmi@orange.fr>
2019-06-26 15:51:00 +0200

    #132 - Alert refacto auto hide duration - Achievement

--- src/screens/AchievementScreen.js
+++ src/screens/AchievementScreen.js
@@ -3,34 +3,19 @@ import HeroBanner from "../components/HeroBanner";
 import { FormattedMessage } from 'react-intl';
 import AchievementForm from "../components/form/AchievementForm";
 import Helper from "../components/feature/cho/Helper";
-import { connect } from 'react-redux';
-import AlertMessage from "../components/AlertMessage";
 
-class AchievementScreen extends React.Component {
+export default class AchievementScreen extends React.Component {
 
   render() {
-
-    const { alert } = this.props;
 
     return (
       <div>
         <HeroBanner title={<FormattedMessage id='chotavern' defaultMessage="Cho's Tavern" />}
         description={<FormattedMessage id="title.achievement" defaultMessage="Do you want me to sing your legend?" />}/>
         <p><FormattedMessage id='character.helper' defaultMessage="Please fill your server and the name of your character." /></p>
-        {/* --- System for alert --- */}
-        {alert.message && <AlertMessage message={alert.message} type={alert.type}/>}
         <AchievementForm />
         <Helper />
       </div>
     );
   }
 }
-
-function mapStateToProps(state) {
-  const { alert } = state;
-  return {
-    alert,
-  };
-}
-
-export default connect(mapStateToProps)(AchievementScreen);
