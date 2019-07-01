c2cbf3123b6c7240223891499bd7e52388971c6e
AxelE <axel91evrard@gmail.com>
2019-07-01 23:53:30 +0200

    update log

--- src/index.js
+++ src/index.js
@@ -62,10 +62,10 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("./firebase-messaging-sw.js")
     .then(function(registration) {
-      console.log("Registration successful, scope is:", registration.scope);
+      console.log("Push notification available, scope is:", registration.scope);
     })
     .catch(function(err) {
-      console.log("Service worker registration failed, error:", err);
+      console.log("Push notification not available, error:", err);
     });
 }
 
