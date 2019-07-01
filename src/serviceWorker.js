b7dca4d8adca5e300445b39a796a6b67ef7746ec
AxelE <axel91evrard@gmail.com>
2019-07-01 23:14:31 +0200

    Add to homescreen

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -116,7 +116,19 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI notify the user they can add to home screen
-  if(e){
-    console.log(deferredPrompt);
-  }
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
+
+window.addEventListener('appinstalled', (evt) => {
+  console.log('a2hs installed');
 });
