97c95783705390e3bc6441a4db8fc993c89b09e2
Roman DILMI <roman.dilmi@orange.fr>
2019-07-02 12:07:36 +0200

    #150 - Init page profile like login

---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../components/Login";
+
+export default class ProfileScreen extends React.Component {
+
+  render() {
+
+    return (
+      <div className="LoginScreen">
+        <Login/>
+      </div>
+    );
+  }
+}
